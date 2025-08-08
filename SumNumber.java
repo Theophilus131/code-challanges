8f79a742979fbfd582df6a2a98213b4fdf4273d7
Theophilus Danladi Umar <theophilusumar27@gmail.com>
2025-08-08 23:03:00 +0100

    Create SumNumber.java
    suming numbers from 1 to 10

---
+++ SumNumber.java
@@ -0,0 +1,20 @@
+
+
+public class SumNumber{
+public static void main(String [] args){
+
+int total = 0;
+
+for(int index = 1; index < 11; index++){
+
+	total += index;
+
+	}
+	
+	System.out.println("Sum of number " + total);
+	
+}
+
+
+
+	}
