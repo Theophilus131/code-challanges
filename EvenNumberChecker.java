930246e8e54a84b73f83131e9a96cc9726fc2906
Theophilus Danladi Umar <theophilusumar27@gmail.com>
2025-08-08 23:13:59 +0100

    Create EvenNumberChecker.java
    even numbers

---
+++ EvenNumberChecker.java
@@ -0,0 +1,14 @@
+// Question 24
+
+	public class EvenNumberChecker {
+
+    	public static void main(String[] args) {
+
+        for (int number = 1; number <= 10; number++) {
+            if (number % 2 == 0) {
+                System.out.println(number);
+            }
+        }
+
+    }
+}
