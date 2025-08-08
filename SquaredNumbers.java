a6e54327a343ff6c2df9761012a8baf9e95a49c8
Theophilus Danladi Umar <theophilusumar27@gmail.com>
2025-08-08 23:03:20 +0100

    Create SquaredNumbers.java
    squaring numbers from 1 to 5

---
+++ SquaredNumbers.java
@@ -0,0 +1,13 @@
+// Question 10
+
+   public class SquaredNumbers {
+
+    public static void main(String[] args) {
+
+        for (int number = 1; number <= 5; number++) {
+            int square = number * number;
+            System.out.println(number + " squared is " + square);
+        }
+
+    }
+}
