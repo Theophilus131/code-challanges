d23e547b15187ea27f4879fdd5a77c77c53e5e61
Theophilus Danladi Umar <theophilusumar27@gmail.com>
2025-08-08 23:13:30 +0100

    Create Counting.java
    displaying a number 5times

---
+++ Counting.java
@@ -0,0 +1,18 @@
+ // Question 4
+
+public class Counting{
+public static void main(String [] args){
+
+
+
+        for (int index = 0; index < 25; index++) {
+            int number = (index % 5) + 1;
+            System.out.println("Number: " + number);
+        }
+
+  
+
+	}
+
+		}
+
