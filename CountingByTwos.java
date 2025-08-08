d3ff8efd2ed44ea3f84645054a05fb1514f07278
Theophilus Danladi Umar <theophilusumar27@gmail.com>
2025-08-08 23:13:01 +0100

    Create CountingByTwos.java
    adding number by two

---
+++ CountingByTwos.java
@@ -0,0 +1,15 @@
+// Question 16
+
+
+	public class CountingByTwos {
+
+    	public static void main(String[] args) {
+
+        for (int number = 2; number <= 10; number += 2) {
+            System.out.println(number);
+        }
+
+    }
+}
+
+
