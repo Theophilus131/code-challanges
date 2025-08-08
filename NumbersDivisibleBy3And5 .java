52f84d01501468402458c9663cc3996e1b161404
Theophilus Danladi Umar <theophilusumar27@gmail.com>
2025-08-08 23:10:39 +0100

    Create NumbersDivisibleBy3And5 .java
    numbers divisible by  3 and 5

---
+++ NumbersDivisibleBy3And5 .java
@@ -0,0 +1,15 @@
+//Question 27
+
+
+	public class NumbersDivisibleBy3And5 {
+
+    	public static void main(String[] args) {
+
+        for (int number = 1; number <= 20; number++) {
+            if (number % 3 == 0 && number % 5 == 0) {
+                System.out.println(number);
+           	 }
+        		}
+
+    }
+}
