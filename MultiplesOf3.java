9eeb6fd845c60f9915d9948a079411cb9d080c11
Theophilus Danladi Umar <theophilusumar27@gmail.com>
2025-08-08 23:11:57 +0100

    Create MultiplesOf3.java
    adding number from 3 to 15

---
+++ MultiplesOf3.java
@@ -0,0 +1,20 @@
+ 
+// Question 6
+
+
+import java.util.Scanner;
+
+public class MultiplesOf3{
+
+public static void main(String [] args){
+
+
+        for (int number = 3; number <= 15; number += 3) {
+            System.out.println(number);
+        }
+
+    
+
+}
+
+	}
