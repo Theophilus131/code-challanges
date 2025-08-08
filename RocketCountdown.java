206ca0b056268d3a819060e9e911be5f869ba677
Theophilus Danladi Umar <theophilusumar27@gmail.com>
2025-08-08 23:04:35 +0100

    Create RocketCountdown.java
    counting down number from 20 to 10

---
+++ RocketCountdown.java
@@ -0,0 +1,17 @@
+//Question 40
+
+
+	public class RocketCountdown {
+
+    	public static void main(String[] args) {
+
+        for (int number = 20; number >= 10; number--) {
+            System.out.println(number);
+
+            if (number == 15) {
+                System.out.println("Halfway!");}
+            
+       		 }
+
+    }
+}
