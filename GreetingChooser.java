8fa74773c4471f53920cfd90ebeef2b4def03b32
Theophilus Danladi Umar <theophilusumar27@gmail.com>
2025-08-08 23:17:56 +0100

    Create GreetingChooser.java
    a logic to ask for a name of user and print greating

---
+++ GreetingChooser.java
@@ -0,0 +1,22 @@
+
+// Question 1
+import java.util.Scanner;
+
+public class GreetingChooser{
+public static void main(String [] args){
+
+	Scanner input = new Scanner(System.in);
+
+	System.out.println("type in your name ");
+	String name = input.next();
+	
+	if(name == "Alex"){
+
+	System.out.println("Hello, friend! :");
+	}
+	else{System.out.println("Hi, Stranger! ")}
+		
+
+	}
+
+		}
