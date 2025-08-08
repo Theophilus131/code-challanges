458f2ef0c1b44cf51ad35559f22798715b082c30
Theophilus Danladi Umar <theophilusumar27@gmail.com>
2025-08-08 22:57:30 +0100

    Create TimesTable5.java
    the 5 times multiplication table

---
+++ TimesTable5.java
@@ -0,0 +1,14 @@
+// Question  22
+
+
+	public class TimesTable5 {
+
+    public static void main(String[] args) {
+
+        for (int number = 1; number <= 10; number++) {
+            int result = 5 * number;
+            System.out.println("5 x " + number + " = " + result);
+        }
+
+    }
+}
