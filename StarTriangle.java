341c1e007b5d84f45c5744aaaffa6be0c60ae2f7
Theophilus Danladi Umar <theophilusumar27@gmail.com>
2025-08-08 23:06:11 +0100

    Create StarTriangle.java
    printing of star

---
+++ StarTriangle.java
@@ -0,0 +1,15 @@
+// Question 26
+
+	public class StarTriangle {
+
+    	public static void main(String[] args) {
+
+        String stars = "";  
+
+        for (int i = 1; i <= 5; i++) {
+            stars += "*";  
+            System.out.println(stars);
+        }
+
+    }
+}
