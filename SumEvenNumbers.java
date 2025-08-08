8a20ba936a81aead649343df5fc8fc9114c3d010
Theophilus Danladi Umar <theophilusumar27@gmail.com>
2025-08-08 23:02:36 +0100

    Create SumEvenNumbers.java
    sum of even numbers

---
+++ SumEvenNumbers.java
@@ -0,0 +1,34 @@
+
+//Question 32
+	public class SumEvenNumbers{
+	
+	public static void main(String [] args){
+
+
+	int sumOfEvenNumber = 0;
+
+
+	for(int index = 1; index < 21; index++){
+
+	if(index % 2 == 0){
+
+	sumOfEvenNumber += index;
+	}
+	
+	
+
+	}
+
+
+	System.out.println("sum of even numbers "+ sumOfEvenNumber);
+
+
+
+
+		}
+
+
+
+
+
+	}
