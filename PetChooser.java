9c39f7776580c79a65aa4e6c063da7db0074641f
Theophilus Danladi Umar <theophilusumar27@gmail.com>
2025-08-08 23:10:18 +0100

    Create PetChooser.java
    checking to see if animal is dog or not

---
+++ PetChooser.java
@@ -0,0 +1,26 @@
+
+//Question 2
+
+import java.util.Scanner;
+
+public class PetChooser{
+public static void main(String [] args){
+
+
+	Scanner input = new Scanner(System.in);
+
+	System.out.println("Type in your favorite pet :");
+	String userChoice = input.next();
+
+	
+	if(userChoice == "dog"){
+	System.out.println("woof dogs are awesome ");}
+
+	else{System.out.println("Cool choice, but i love dogs ");}
+	
+	
+}
+
+
+
+	}
