6474ee691e91ad15a47ac6fd4986d82b7a8818e7
Theophilus Danladi Umar <theophilusumar27@gmail.com>
2025-08-08 23:15:16 +0100

    Create LoopingStars.java
    displaying pattern with star

---
+++ LoopingStars.java
@@ -0,0 +1,12 @@
+//Question 12
+
+	public class LoopingStars {
+
+   	 public static void main(String[] args) {
+
+        for (int count = 1; count <= 5; count++) {
+            System.out.println("*");
+        }
+
+    }
+}
