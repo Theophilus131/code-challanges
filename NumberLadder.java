27269de04260bebe6bfdc38de3936da3d794c432
Theophilus Danladi Umar <theophilusumar27@gmail.com>
2025-08-08 23:19:22 +0100

    Create NumberLadder.java
    counting number from 1 to 7

---
+++ NumberLadder.java
@@ -0,0 +1,12 @@
+//Question 20
+
+	public class NumberLadder {
+
+    	public static void main(String[] args) {
+
+        for (int number = 1; number <= 7; number++) {
+            System.out.println(number);
+        }
+
+    }
+}
